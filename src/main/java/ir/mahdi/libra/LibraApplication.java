4a67f4730dd1993a52f3a03649a4504956625ab5
mahdi <mm.rajabi@mahsan.co>
2025-03-01 12:45:25 +0330

    initialize Spring Boot application with basic configuration and Maven wrapper

---
+++ src/main/java/ir/mahdi/libra/LibraApplication.java
@@ -0,0 +1,13 @@
+package ir.mahdi.libra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraApplication.class, args);
+    }
+
+}
