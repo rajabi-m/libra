25175622358e0117a3d5964ad8b4aaeaefe18c52
mahdi <mm.rajabi@mahsan.co>
2025-03-02 15:05:20 +0330

    feat: add debug mode to application

--- src/main/java/ir/mahdi/libra/exception/GlobalExceptionHandler.java
+++ src/main/java/ir/mahdi/libra/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package ir.mahdi.libra.exception;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -14,10 +15,18 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final Environment environment;
+
+    public GlobalExceptionHandler(Environment environment) {
+        this.environment = environment;
+    }
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         Map<String, List<String>> body = new HashMap<>();
@@ -38,10 +47,19 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(Error.of(ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> handleException(Exception ex) {
+        if (Objects.equals(environment.getProperty("application.debug"), "true")) {
+            throw new RuntimeException(ex);
+        }
+        return new ResponseEntity<>(Error.of("Internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
     public record Error(List<String> errors) {
         public static Error of(String message) {
             return new Error(List.of(message));
         }
     }
-
 }
