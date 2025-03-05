3d12417a45507b4d3825478358e7ebccaa216bdb
mahdi <mm.rajabi@mahsan.co>
2025-03-05 12:58:20 +0330

    feat: update BorrowableAsset and User models to support multiple borrowed assets per user

--- src/main/java/ir/mahdi/libra/model/User.java
+++ src/main/java/ir/mahdi/libra/model/User.java
@@ -18,8 +18,8 @@ public class User {
     private Long id;
     private String username;
 
-    @OneToOne(mappedBy = "borrower")
-    private BorrowableAsset borrowedAsset;
+    @OneToMany(mappedBy = "borrower")
+    private List<BorrowableAsset> borrowedAssets;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<BorrowHistory> borrowHistory;
